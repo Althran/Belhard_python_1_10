caf68eaf74df9853987a538df86539f12aef0535
Althran <overlord106@gmail.com>
2022-08-20 12:59:30 +0300

    Complete tasks

--- tasks/csv_tasks/population.py
+++ tasks/csv_tasks/population.py
@@ -3,3 +3,27 @@
 файла world_population.csv и функцию должна возвращать год, в котором
 наибольший процент прироста населения.
 """
+from pathlib import Path
+import csv
+
+
+def max_delta(find_population):
+    max_count = 0
+    with open(find_population, 'r') as f_p:
+        reader = csv.reader(f_p)
+        count = 0
+        find_year = 0
+        for row in reader:
+            if count == 0:
+                pass
+            else:
+                number = float(row[2])
+                if number > max_count:
+                    max_count = number
+                    find_year = int(row[0])
+            count += 1
+
+        print(str(find_year))
+
+
+max_delta(Path('world_populations1.csv'))
--- tasks/file_tasks/reverse_rows.py
+++ tasks/file_tasks/reverse_rows.py
@@ -3,3 +3,28 @@
 файл. Создать новый файл, каждая строка которого получается из соответствующей
 строки исходного файла перестановкой слов в обратном порядке.
 """
+from pathlib import Path
+
+
+def revert_rows(file_text, new_text):
+    with open(file_text, 'r+') as f, open(new_text, 'w') as f_new:
+        for i in f:
+            f_new.writelines(right_rows(i) + '\n')
+
+
+def right_rows(line):
+    line = line.split()
+    new_dict = {}
+    for idx, i in enumerate(line):
+        if i[-1] in ',.':
+            new_dict[idx] = i[-1]
+        line[idx] = i.strip(',.')
+    line.reverse()
+    for k, v in new_dict.items():
+        line[k] += v
+    line = ' '.join(line)
+    return '. '.join(map(lambda s: s.strip().capitalize(), line.split('.')))
+
+
+revert_rows(Path('text.txt'), Path('text1.txt'))
+
